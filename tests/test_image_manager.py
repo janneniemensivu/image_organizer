e776f2a336d00cc3684d6216f015f0f05e7f8dd1
janneniemensivu <janne.niemensivu@gmail.com>
2023-04-14 14:26:20 +0300

    test_image_manager passed tests

--- tests/test_image_manager.py
+++ tests/test_image_manager.py
@@ -4,62 +4,47 @@ import unittest
 
 from image_manager import ImageManager
 from exif_reader import InvalidFileException
+from PIL import Image
+
 
 class TestImageManager(unittest.TestCase):
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.src_directory = os.path.join("chatGPT", "source")
+        self.dst_directory = os.path.join("chatGPT", "destination")
+        
     def setUp(self):
-        self.test_data_folder = os.path.join(os.getcwd(), "tests", "test_data")
-        self.src_folder = os.path.join(self.test_data_folder, "source")
-        self.dst_folder = os.path.join(self.test_data_folder, "destination")
-        os.makedirs(self.src_folder, exist_ok=True)
-        os.makedirs(self.dst_folder, exist_ok=True)
+        test_files = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'unknown_data.txt', 'video1.mp4']
 
-        # Create test files
-        self.test_files = ["image1.jpg", "image2.jpg", "image3.jpg", "video1.mp4", "unknown_data.txt"]
-        for test_file in self.test_files:
-            test_file_path = os.path.join(self.src_folder, test_file)
-            open(test_file_path, "w").close()
+        # create source and destination directories
+        os.makedirs(self.src_directory, exist_ok=True)
+        os.makedirs(os.path.join(self.dst_directory, "images"), exist_ok=True)
+        os.makedirs(os.path.join(self.dst_directory, "unknown"), exist_ok=True)
+        os.makedirs(os.path.join(self.dst_directory, "videos"), exist_ok=True)
 
-        # Create test images with EXIF data
-        self.exif_test_files = ["image1.jpg", "image2.jpg", "image3.jpg"]
-        for test_file in self.exif_test_files:
-            test_file_path = os.path.join(self.src_folder, test_file)
-            shutil.copyfile(os.path.join(self.test_data_folder, test_file), test_file_path)
-
-        self.image_manager = ImageManager(self.src_folder, self.dst_folder)
-
-    def test_process_image_folder(self):
-        self.image_manager.process_files()
-
-        # Check that source files have been removed
-        self.assertEqual(len(os.listdir(self.src_folder)), 0)
-
-        # Check that files have been moved to the correct destination folders
-        self.assertEqual(os.listdir(self.dst_folder), ["images", "videos", "unknown"])
-        self.assertEqual(os.listdir(os.path.join(self.dst_folder, "images")), ["2011", "2012"])
-        self.assertEqual(os.listdir(os.path.join(self.dst_folder, "images", "2011")), ["image1.jpg"])
-        self.assertEqual(os.listdir(os.path.join(self.dst_folder, "images", "2012")), ["image2.jpg", "image3.jpg"])
-
-        # Check that invalid files have not been moved
-        self.assertEqual(os.listdir(os.path.join(self.dst_folder, "videos")), ["video1.mp4"])
-        self.assertEqual(os.listdir(os.path.join(self.dst_folder, "unknown")), ["unknown_data.txt"])
-
-        # Check that the files with EXIF data have been moved correctly
-        self.assertEqual(os.listdir(os.path.join(self.dst_folder, "images", "2011")), ["image1.jpg"])
-        self.assertEqual(os.listdir(os.path.join(self.dst_folder, "images", "2012")), ["image2.jpg", "image3.jpg"])
-        self.assertEqual(self.image_manager.exif_data, {
-            "image1.jpg": {"DateTimeOriginal": "2011:05:15 12:30:00"},
-            "image2.jpg": {"DateTimeOriginal": "2012:06:18 13:45:00"},
-            "image3.jpg": {"DateTimeOriginal": "2012:06:18 14:00:00"}
-        })
+        # move test files to source directory
+        for filename in test_files:
+            src_path = os.path.join("tests", "test_data", filename)
+            dst_path = os.path.join(self.src_directory, filename)
+            shutil.copy(src_path, dst_path)
 
     def test_process_image_file(self):
-        image_file = os.path.join(self.src_folder, "image1.jpg")
-        self.image_manager.process_image_file(image_file)
+        image_manager = ImageManager(self.src_directory, self.dst_directory)
+        image_manager.process_image_file(os.path.join(self.src_directory, "image1.jpg"))
 
-        # Check that source file has been removed
-        self.assertEqual(len(os.listdir(self.src_folder)), 4)
+        self.assertEqual(len(os.listdir(os.path.join(self.dst_directory, "images", "2011"))), 1)
 
-        # Check that file has been moved to the correct destination folder
-        self.assertEqual(os.listdir(self.dst_folder), ["images"])
-        self.assertEqual(os.listdir(os.path.join(self.dst_folder, "images")), ["2011"])
+    def test_process_image_folder(self):
+        image_manager = ImageManager(self.src_directory, self.dst_directory)
+        image_manager.process_files()
+
+        self.assertEqual(len(os.listdir(os.path.join(self.dst_directory, "images", "2011"))), 1)
+        self.assertEqual(len(os.listdir(os.path.join(self.dst_directory, "images", "2012"))), 1)
+
+    def tearDown(self):
+        shutil.rmtree(self.src_directory)
+        shutil.rmtree(self.dst_directory)
+
+
+if __name__ == "__main__":
+    unittest.main()
